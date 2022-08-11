fe87720a2b92534c51dedeb215c4ac8247414a8c
Fernando Chorney <fernando.chorney@invenia.ca>
2022-08-10 22:02:38 -0500

    Basic button functionality is now done

--- code.py
+++ code.py
@@ -1,9 +1,10 @@
 import board
-import digitalio
 import usb_hid
+import time
 from adafruit_debouncer import Debouncer, ticks_diff, ticks_ms
 from adafruit_hid.keyboard import Keyboard as adaKB
 from adafruit_hid.keycode import Keycode
+from digitalio import DigitalInOut, Pull
 
 LIGHT_PINS = [board.GP11, board.GP15, board.GP16, board.GP20]
 # Set Light Pins to On
@@ -11,84 +12,110 @@ LIGHT_PINS = [board.GP11, board.GP15, board.GP16, board.GP20]
 
 def initialize_lights(pins):
     for pin in pins:
-        p = digitalio.DigitalInOut(pin)
+        p = DigitalInOut(pin)
         p.switch_to_output()
         p.value = True
 
 
-class Button(Debouncer):
-    def __init__(self, pin, short_duration_ms=50, active_down=False, **kwargs):
-        self.short_duration_ms = short_duration_ms
-        self.active_down = active_down
-        self.last_change_ms = ticks_ms()
-        self.short_registered = False
-        self.short_showed = False
-        self.key_pressed = False
-        super().__init__(pin, **kwargs)
+class KeyState(object):
+    IDLE = 0
+    PRESS = 1
+    PRESSED = 2
+    HOLD = 3
+    RELEASE = 4
+    RELEASED = 5
 
-    def _pushed(self):
-        return (self.active_down and super().fell) or (
-            not self.active_down and super().rose
-        )
 
-    def _released(self):
-        return (self.active_down and super().rose) or (
-            not self.active_down and super().fell
-        )
+class Button(object):
+    def __init__(
+        self,
+        pin: int,
+        kb,
+        kcode: int,
+        press_time: float = 0.05,
+        release_time: float = 0.02,
+    ) -> None:
+        self.kcode = kcode
+        self.kstate = KeyState.IDLE
+        self.on = False
+        self.state_changed = False
 
-    def update(self):
-        super().update()
-        if self._pushed():
-            self.last_change_ms = ticks_ms()
-        elif self._released():
-            self.last_change_ms = ticks_ms()
-            if self.short_registered:
-                self.short_registered = False
-                self.short_showed = False
-        else:
-            duration = ticks_diff(ticks_ms(), self.last_change_ms)
-            if (
-                not self.short_registered
-                and self.value != self.active_down
-                and duration > self.short_duration_ms
-            ):
-                self.short_registered = True
+        self.press_time = press_time
+        self.press_timer = 0
+        self.release_time = release_time
+        self.release_timer = 0
 
-    def press(self, kb, key):
-        if self.short_registered and not self.short_showed:
-            self.short_showed = True
+        # Set Pin to Input
+        self.btn = DigitalInOut(pin)
+        self.btn.switch_to_input(pull=Pull.DOWN)
 
-            # Press Here
-            self.key_pressed = True
-            kb.press(key)
-            print(f"pressing {key}")
+        self.kb = kb
+        self.pressed = False
 
-        if self.key_pressed:
-            # Release Here
-            kb.release(key)
-            print(f"releasing {key}")
-            self.key_pressed = False
+    @property
+    def off(self) -> bool:
+        return not self.on
+
+    def press(self) -> None:
+        if not self.pressed:
+            self.kb.press(self.kcode)
+            self.pressed = True
+
+    def release(self) -> None:
+        if self.pressed:
+            self.kb.release(self.kcode)
+            self.pressed = False
+
+    def update(self) -> None:
+        self.on = self.btn.value
+        self.state_changed = False
+
+        if self.kstate == KeyState.IDLE:
+            if self.on:
+                self.transition_to(KeyState.PRESS)
+                self.press_timer = time.monotonic()
+            else:
+                self.release()
+        elif self.kstate == KeyState.PRESS:
+            if (time.monotonic() - self.press_timer) > self.press_time:
+                self.transition_to(KeyState.PRESSED)
+            elif self.off:
+                self.transition_to(KeyState.RELEASE)
+        elif self.kstate == KeyState.PRESSED:
+            self.transition_to(KeyState.HOLD)
+        elif self.kstate == KeyState.HOLD:
+            if self.off:
+                self.transition_to(KeyState.RELEASE)
+                self.release_timer = time.monotonic()
+            else:
+                self.press()
+        elif self.kstate == KeyState.RELEASE:
+            if (time.monotonic() - self.release_timer) > self.release_time:
+                self.transition_to(KeyState.RELEASED)
+            elif self.on:
+                self.transition_to(KeyState.PRESS)
+        elif self.kstate == KeyState.RELEASED:
+            self.transition_to(KeyState.IDLE)
+
+    def transition_to(self, next_state: KeyState) -> None:
+        self.kstate = next_state
+        self.state_changed = True
 
 
 class Controller:
     def __init__(self):
-        self.left = Button(digitalio.DigitalInOut(board.GP10))
-        self.right = Button(digitalio.DigitalInOut(board.GP21))
-        self.start = Button(digitalio.DigitalInOut(board.GP17))
-        self.enter = Button(digitalio.DigitalInOut(board.GP14))
+        ada_kbd = adaKB(usb_hid.devices)
 
-        self.ada_kbd = adaKB(usb_hid.devices)
+        self.left = Button(board.GP10, ada_kbd, Keycode.FOUR)
+        self.right = Button(board.GP17, ada_kbd, Keycode.FIVE)
+        self.start = Button(board.GP21, ada_kbd, Keycode.SIX)
+        self.enter = Button(board.GP14, ada_kbd, Keycode.SEVEN)
 
     def update(self):
         self.left.update()
         self.right.update()
         self.start.update()
         self.enter.update()
-
-        self.left.press(self.ada_kbd, Keycode.FOUR)
-        self.right.press(self.ada_kbd, Keycode.FIVE)
-        self.start.press(self.ada_kbd, Keycode.SIX)
-        self.enter.press(self.ada_kbd, Keycode.SEVEN)
 
 
 try:
